b85ca17e06b1adfec8855a6b63da18a2a43bd62f
Jean-Luc Thirion <thirion.jl@gmail.com>
2018-12-12 00:14:32 +0100

    Implement conv2D

--- chains/core/ops_conv.py
+++ chains/core/ops_conv.py
@@ -1,60 +1,17 @@
-from typing import Iterable, Union
-
 import numpy as np
 
+from chains.core import utils_conv as uc
 from chains.core.ops import Op
 from chains.core.shape import StaticShape
 from chains.core.tensor import Tensor
-from chains.utils import validate
 
 __all__ = ["Conv2D"]
-
-
-class ConvFormat:
-    Transposable = Union[Tensor, StaticShape]
-    Perm = Iterable[int]
-
-    def __init__(self, features_perm: Perm, filters_perm: Perm):
-        validate.is_permutation(features_perm, 4)
-        validate.is_permutation(filters_perm, 4)
-        self.features_perm = features_perm
-        self.filters_perm = filters_perm
-
-    def nchw(self, a: Transposable) -> Transposable:
-        return a.transpose(self.features_perm)
-
-    def nchw_inv(self, a: Transposable) -> Transposable:
-        return a.transpose(self._inverse_feature_perm())
-
-    def dchw(self, f: Transposable) -> Transposable:
-        return f.transpose(self.filters_perm)
-
-    def dchw_inv(self, f: Transposable) -> Transposable:
-        return f.transpose(self._inverse_filter_perm())
-
-    def _inverse_filter_perm(self: Perm) -> Perm:
-        return self.inverse_perm(self.filters_perm)
-
-    def _inverse_feature_perm(self: Perm) -> Perm:
-        return self.inverse_perm(self.features_perm)
-
-    @staticmethod
-    def inverse_perm(perm):
-        return np.argsort(perm)
-
-    @staticmethod
-    def apply_perm(f, perm):
-        return tuple(f[perm[i]] for i in range(len(perm)))
-
-
-NCHW = ConvFormat((0, 1, 2, 3), (0, 1, 2, 3))
-CHWN = ConvFormat((3, 0, 1, 2), (0, 1, 2, 3))
 
 
 class Conv2D(Op):
 
     def __init__(self, feature_derivative=True, padding=0, stride=1,
-                 conv_format=NCHW):
+                 conv_format=uc.NCHW):
         super().__init__()
         self.feature_derivative = feature_derivative
         self.conv_format = conv_format
@@ -65,7 +22,6 @@ class Conv2D(Op):
         self.bias = None
         self.xc = None
         self.fc = None
-        self.zc = None
 
     def check_incoming_shapes(self, features: StaticShape,
                               filters: StaticShape, b: StaticShape):
@@ -78,11 +34,13 @@ class Conv2D(Op):
                              f"but got {filters.ndim} dimensions")
 
         d, c, fh, fw = self.conv_format.dchw(filters)
-        m, s, nh, nw = self.conv_format.nchw(features)
+        m, cc, nh, nw = self.conv_format.nchw(features)
+        p = self.padding
+        s = self.stride
 
-        if c != s:
+        if c != cc:
             raise ValueError(f"Number of channels should be the same "
-                             f"in features({s}) and filters({c})")
+                             f"in features({cc}) and filters({c})")
 
         if not (b.is_column()):
             raise ValueError(f"Bias should be a 2-D column vector, got {b}")
@@ -91,192 +49,92 @@ class Conv2D(Op):
             raise ValueError(f"Number of bias should match number of filters "
                              f"but got {b[0]} and {d}")
 
+        if (nh.value + 2 * p) % s != 0:
+            raise ValueError(
+                f"Padded height ({nh + 2 * p}) should be a multiple of "
+                f"stride {cc} but is not")
+
+        if (nw.value + 2 * p) % s != 0:
+            raise ValueError(
+                f"Padded width ({nh + 2 * p}) should be a multiple of "
+                f"stride {cc} but is not")
+
     def compute_out_shape(self, features: StaticShape, filters: StaticShape,
                           biases: StaticShape) -> StaticShape:
 
         d, c, fh, fw = self.conv_format.dchw(filters)
         m, s, nh, nw = self.conv_format.nchw(features)
-        out_h, out_w = _clip_positions_count(nh.value, nw.value, fh.value,
-                                             fw.value, self.padding,
-                                             self.stride)
+        out_h, out_w = uc.clip_positions_count(nh.value, nw.value, fh.value,
+                                               fw.value, self.padding,
+                                               self.stride)
 
         tup = self.conv_format.nchw_inv(StaticShape(m, d, out_h, out_w))
         return StaticShape.from_tuple(tup)
 
     def compute(self, features: Tensor, filters: Tensor, bias: Tensor):
-        self.features = self.conv_format.to_nchw(features)
-        self.filters = self.conv_format.to_dchw(filters)
+        self.features = self.conv_format.nchw(features)
+        self.filters = self.conv_format.dchw(filters)
         self.bias = bias
 
-        xc = im2col_activations(features, filters, self.padding, self.stride)
-        fc = im2col_filters(features, filters)
-        zc = fc @ xc + bias
+        self.xc = uc.im2col(self.features, self.filters, self.padding,
+                            self.stride)
+        self.fc = uc.im2col_filters(self.filters)
+        zc = self.fc @ self.xc + bias
 
-        self.xc = xc
-        self.fc = fc
-        self.zc = zc
-        out = reshape_out(zc, features, filters, self.padding, self.stride)
-        self.output = self.conv_format.undo_to_nchw(out)
+        out = self._reshape_out(zc)
+        self.output = self.conv_format.nchw_inv(out)
 
     def partials(self, d_output):
-        m, c, nh, nw, d, fh, fw = _all_dimensions(self.features, self.filters)
-        mm, dd, cnt_h, cnt_w = d_output.shape
+        m, c, nh, nw, d, fh, fw = self._dimensions()
+
+        d_out = self.conv_format.nchw(d_output)
+        mm, dd, cnt_h, cnt_w = d_out.shape
 
         assert mm == m, f"Number of examples im activations({m}) is " \
             f"different from number of examples in output derivatives({mm})"
         assert dd == d, f"Number of filters in input({d}) is " \
             f"different from number of filters in output derivatives({dd})"
 
-        dzc = d_output.transpose(0, 1, 2, 3).reshape(d, m * cnt_h * cnt_w)
-        db = np.sum(d_output, axis=1, keepdims=True)
-        dfc = d_output @ self.features.T
-        dxc = self.weights.T @ d_output if self.feature_derivative else 0
+        dzc = self._reshape_out_back(d_out)
 
-        df = dfc.reshape(c, c, fh, fw)
-        dx = col2im_activations(dxc, self.features, self.features,
-                                self.padding, self.stride)
+        db = np.sum(dzc, axis=1, keepdims=True)
+        dfc = dzc @ self.xc.T
+        dxc = self.fc.T @ dzc if self.feature_derivative else 0
 
-        return self.conv_format.undo_to_nchw(
-            dx), self.conv_format.undo_to_dchw(df), db
+        df = uc.col2im_filters(dfc, self.filters)
+        dx = uc.col2im(dxc, self.features, self.filters, self.padding,
+                       self.stride)
 
+        return self.conv_format.nchw_inv(dx), self.conv_format.dchw_inv(df), db
 
-def _all_dimensions(activations, filters):
-    if activations.ndim != 4:
-        raise ValueError(f"activations should be a 4 dimensional tensor "
-                         f"but got {activations.ndim} dimensions")
-    if filters.ndim != 4:
-        raise ValueError(f"filters should be a 4 dimensional tensor "
-                         f"but got {filters.ndim} dimensions")
+    def _reshape_out(self, zc):
+        """Reshapes ZC output to Z"""
+        m, c, nh, nw, d, fh, fw = self._dimensions()
+        cnt_h, cnt_w = uc.clip_positions_count(nh, nw, fh, fw, self.padding,
+                                               self.stride)
 
-    d, c, fh, fw = filters.shape
-    m, s, nh, nw = activations.shape
+        assert zc.shape == (d, m * cnt_h * cnt_w)
+        return zc.reshape((d, m, cnt_h, cnt_w)).transpose(1, 0, 2, 3)
 
-    if c != s:
-        raise ValueError(f"Number of channels should be the same "
-                         f"in activations({s}) and filters({c})")
+    def _reshape_out_back(self, dz):
+        """Reshapes dZC to dZ"""
+        m, c, nh, nw, d, fh, fw = self._dimensions()
+        cnt_h, cnt_w = uc.clip_positions_count(nh, nw, fh, fw, self.padding,
+                                               self.stride)
 
-    return m, c, nh, nw, d, fh, fw
+        assert dz.shape == (m, d, cnt_h, cnt_w)
+        return dz.transpose(1, 0, 2, 3).reshape((d, m * cnt_h * cnt_w))
 
+    def _dimensions(self):
+        assert self.features.ndim == 4, f"activations should be a 4 " \
+            f"dimensional tensor but got {self.v.ndim} dimensions"
+        assert self.filters.ndim == 4, f"filters should be a 4 dimensional " \
+            f"tensor but got {self.filters.ndim} dimensions"
 
-# From https://github.com/wiseodd/hipsternet/blob/master/hipsternet/im2col.py
-def _im2col_indices(nh, nw, fh, fw, c, padding=0, stride=1):
-    out_h, out_w = _clip_positions_count(nh, nw, fh, fw, padding, stride)
+        d, c, fh, fw = self.filters.shape
+        m, s, nh, nw = self.features.shape
 
-    i_rows = np.tile(np.repeat(np.arange(fh), fw), c)
-    i_cols = stride * np.repeat(np.arange(out_h), out_w)
-    i = i_rows.reshape(-1, 1) + i_cols.reshape(1, -1)
+        assert c == s, f"Number of channels should be the same " \
+            f"in activations({s}) and filters({c})"
 
-    j_rows = np.tile(np.arange(fw), fh * c)
-    j_cols = stride * np.tile(np.arange(out_w), out_h)
-    j = j_rows.reshape(-1, 1) + j_cols.reshape(1, -1)
-
-    k = np.repeat(np.arange(c), fh * fw).reshape(-1, 1)
-    return k.astype(int), i.astype(int), j.astype(int)
-
-
-def im2col_activations(activations, filters, padding=0, stride=1):
-    """
-       Return the 4-D activations tensor as a 2-D tensor XC
-       (c * fh * fw, m * nb_clip_positions), where:
-       M[:, i] represents the "clip" where a filter should be applied but
-       flattened.
-       i = sample_index * n + clip_position where:
-       - n is the number of clipping positions
-       - clip_position is the position of top-left corner, when scanning
-       activations first by rows, then by columns
-
-       :return: activations as a 2-D tensor with dimensions
-       (filter_flattened_size, m * clipping_positions_cnt)
-       """
-    m, c, nh, nw, d, fh, fw = _all_dimensions(activations, filters)
-    activations_padded = pad(activations, padding)
-
-    channels, rows, cols = _im2col_indices(nh, nw, fh, fw, c, padding, stride)
-
-    # im2col_3d.shape = (m, filter_flattened_size, clipping_positions_cnt)
-    im2col_3d = activations_padded[:, channels, rows, cols]
-    im2col_2d = im2col_3d.transpose(1, 2, 0).reshape(c * fh * fw, -1)
-    return im2col_2d
-
-
-def im2col_filters(activations, filters):
-    """Returns filters reshape as a matrix (d, c * fh * fw)"""
-    _, c, _, _, d, fh, fw = _all_dimensions(activations, filters)
-    return filters.reshape(d, c * fh * fw)
-
-
-def pad(activations, padding=0):
-    if padding == 0:
-        return activations
-    else:
-        p = padding
-        return np.pad(activations, ((0, 0), (0, 0), (p, p), (p, p)),
-                      mode='constant')
-
-
-def _clip_positions_count(nh, nw, fh, fw, padding=0, stride=1):
-    """
-    Counts the number of positions where the filters can be applied on
-    activations.
-
-    :return: A pair (number of horizontal positions, number of vertical
-    positions)
-    """
-    if padding >= fh or padding >= fw:
-        raise ValueError(f"Padding({padding}) should be strictly smaller than"
-                         f" filter height({fh}) and width({fw})")
-
-    width = nw + 2 * padding - fw
-    height = nh + 2 * padding - fh
-
-    if height % stride != 0:
-        raise ValueError(f"Padded height ({height}) should be divisible by "
-                         f"stride ({stride} but is not")
-    if width % stride != 0:
-        raise ValueError(f"Padded width ({width}) should be divisible by "
-                         f"stride ({stride} but is not")
-
-    return height // stride + 1, width // stride + 1
-
-
-def col2im_activations(dXC, activations, filters, padding=0, stride=1):
-    m, c, nh, nw, d, fh, fw = _all_dimensions(activations, filters)
-
-    h_padded, w_padded = nh + 2 * padding, nw + 2 * padding
-    activations_padded = np.zeros((m, c, h_padded, w_padded), dtype=dXC.dtype)
-
-    k, i, j = _im2col_indices(nh, nw, fh, fw, c, padding, stride)
-    cols_reshaped = dXC.reshape(c * fh * fw, -1, m)
-    cols_reshaped = cols_reshaped.transpose(2, 0, 1)
-    np.add.at(activations_padded, (slice(None), k, i, j), cols_reshaped)
-
-    if padding == 0:
-        out = activations_padded
-    else:
-        out = activations_padded[:, :, padding:-padding, padding:-padding]
-
-    assert out.shape == activations.shape
-    return out
-
-
-def col2im_filters(dFC, activations, filters):
-    """Returns filters reshape as a matrix (d, c * fh * fw)"""
-    _, c, _, _, d, fh, fw = _all_dimensions(activations, filters)
-    assert dFC.shape == (d, c * fh * fw)
-    return filters.reshape(d, c, fh, fw)
-
-
-def reshape_out(zc, activations, filters, padding=0, stride=1):
-    """Reshapes ZC output to Z"""
-    m, c, nh, nw, d, fh, fw = _all_dimensions(activations, filters)
-    cnt_h, cnt_w = _clip_positions_count(activations, filters, padding, stride)
-    assert zc.shape == (d, m * cnt_h * cnt_w)
-    return zc.reshape((d, m, cnt_h, cnt_w)).transpose(1, 0, 2, 3)
-
-
-def reshape_out_back(dZ, activations, filters, stride=1):
-    """Reshapes dZC to dZ"""
-    (m, c, nh, nw, d, fh, fw) = _all_dimensions(activations, filters)
-    (cnt_h, cnt_w) = _clip_positions_count(activations, filters, stride, 0)
-    assert dZ.shape == (m, d, cnt_h, cnt_w)
-    return dZ.transpose(1, 0, 2, 3).reshape((d, m * cnt_h * cnt_w))
+        return m, c, nh, nw, d, fh, fw
---
+++ tests/core/test_utils_conv.py
@@ -0,0 +1,108 @@
+import numpy as np
+import pytest
+
+import chains.core.utils_conv as uc
+
+
+def test_all_dimensions_without_error():
+    inputs = np.zeros((32, 3, 500, 400))
+    filters = np.zeros((5, 3, 12, 10))
+    assert uc._all_dimensions(inputs, filters) == (32, 3, 500, 400, 5, 12, 10)
+
+
+def test_all_dimensions_with_non_matching_channels():
+    with pytest.raises(ValueError) as ex:
+        uc._all_dimensions(np.zeros((32, 4, 500, 400)),
+                           np.zeros((5, 3, 12, 10)))
+    assert str(ex.value) == 'Number of channels should be the same ' \
+                            'in activations(4) and filters(3)'
+
+
+def test_im2col_1channel_1example_stride1():
+    inputs = np.arange(1, 10).reshape(1, 1, 3, 3)
+    filters = np.zeros((1, 1, 2, 2))
+    np.testing.assert_array_equal(uc.im2col(inputs, filters),
+                                  np.array([[1, 2, 4, 5], [2, 3, 5, 6],
+                                            [4, 5, 7, 8], [5, 6, 8, 9]]))
+
+
+def test_im2col_same_convolution():
+    inputs = np.arange(1, 17).reshape(1, 1, 4, 4)
+    filters = np.zeros((1, 1, 3, 3))
+
+    actual = uc.im2col(inputs, filters, padding=1)
+
+    np.testing.assert_array_equal(actual[:, 0],
+                                  np.array([0, 0, 0, 0, 1, 2, 0, 5, 6]).T)
+    np.testing.assert_array_equal(actual[:, 5],
+                                  np.array([1, 2, 3, 5, 6, 7, 9, 10, 11]).T)
+    np.testing.assert_array_equal(actual[:, -1],
+                                  np.array([11, 12, 0, 15, 16, 0, 0, 0, 0]).T)
+
+
+def test_im2col_1channel_1example_stride2():
+    inputs = np.arange(1, 17).reshape(1, 1, 4, 4)
+    filters = np.zeros((1, 1, 2, 2))
+    np.testing.assert_array_equal(uc.im2col(inputs, filters, stride=2),
+                                  np.array([[1, 2, 5, 6], [3, 4, 7, 8],
+                                            [9, 10, 13, 14],
+                                            [11, 12, 15, 16]]).T)
+
+
+def test_im2col_multi_channel_multi_example_stride1():
+    partial_input = np.arange(1, 28).reshape(3, 3, 3)
+    inputs = np.stack([partial_input, -partial_input])
+    assert inputs.shape == (2, 3, 3, 3)
+    filters = np.zeros((1, 3, 2, 2))
+    e = np.array([[1, 2, 4, 5, 10, 11, 13, 14, 19, 20, 22, 23],
+                  [2, 3, 5, 6, 11, 12, 14, 15, 20, 21, 23, 24],
+                  [4, 5, 7, 8, 13, 14, 16, 17, 22, 23, 25, 26],
+                  [5, 6, 8, 9, 14, 15, 17, 18, 23, 24, 26, 27]])
+    expected = np.concatenate([e, -e]).T
+    np.testing.assert_array_equal(uc.im2col(inputs, filters), expected)
+
+
+def test_im2col():
+    inputs = np.zeros((2, 3, 3, 3), dtype=np.int64)
+    filters = np.zeros((1, 3, 2, 2))
+    partial_cols = np.arange(1, 49).reshape(4, 12).T
+    cols = np.hstack([partial_cols, -partial_cols])
+    partial_expected = np.array([
+        [[1, 15, 14], [28, 82, 54], [27, 67, 40]],
+        [[5, 23, 18], [36, 98, 62], [31, 75, 44]],
+        [[9, 31, 22], [44, 114, 70], [35, 83, 48]]
+    ])
+    expected = np.stack([partial_expected, -partial_expected])
+    np.testing.assert_array_equal(expected,
+                                  uc.col2im(cols, inputs, filters))
+
+
+def test_im2col_padding():
+    inputs = np.zeros((2, 2, 2, 2), dtype=np.int64)
+    filters = np.zeros((1, 2, 2, 2))
+    cols = np.ones(shape=(2 * 2 * 2, 9 * 2), dtype=np.int64)
+    expected = np.ones(shape=(2, 2, 2, 2)) * 4
+    np.testing.assert_array_equal(expected,
+                                  uc.col2im(cols, inputs, filters, padding=1))
+
+
+def test_im2col_stride():
+    inputs = np.zeros((1, 1, 5, 5), dtype=np.int64)
+    filters = np.zeros((1, 1, 3, 3))
+    cols = np.ones(shape=(9, 4), dtype=np.int64)
+    expected = np.array([
+        [1, 1, 2, 1, 1],
+        [1, 1, 2, 1, 1],
+        [2, 2, 4, 2, 2],
+        [1, 1, 2, 1, 1],
+        [1, 1, 2, 1, 1]
+    ]).reshape(1, 1, 5, 5)
+    actual = uc.col2im(cols, inputs, filters, stride=2)
+    np.testing.assert_array_equal(expected, actual)
+
+
+def test_im2col_filter_back_and_forth():
+    f = np.random.randint(1, 10, (2, 3, 2, 2))
+    fc = uc.im2col_filters(f)
+    f_back = uc.col2im_filters(fc, f)
+    np.testing.assert_array_equal(f, f_back)
